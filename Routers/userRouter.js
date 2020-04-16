2f59cf5e45cdaa99c62e6df905314cac479f540a
abigail-17 <kechenz1@student.unimelb.edu.au>
2020-04-16 23:44:25 +1000

    New register and login page created
    functionalities need to be improved !!!!

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const usersRouter = express.Router();
 const userController = require("../Controllers/userController");
+var users = require('../Models/user');
+
 //Route for GET method
 //can also define app.post, app.delete, or other HTTP methods
 usersRouter.get('/', (req, res) => {
     //return the response
     userController.getAllUsers(req, res);
+});
+
+//register form
+usersRouter.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//register process
+usersRouter.get('/register', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+
+    req.checkBody('Name', 'Name cannot be empty').nonEmpty();
+    req.checkBody('email', 'Email cannot be empty').nonEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username cannot be empty').nonEmpty();
+    req.checkBody('password', 'Password is invalid').nonEmpty();
+    req.checkBody('password2', 'Passwords do not matched').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if(errors) {
+        res.render('register', {
+            errors:errors
+        });
+    } else {
+        res.send('Welcome to Mit&Yit! You have successfully registered ;)')
+    }
+});
+
+//login form
+usersRouter.get('/login', (req, res) => {
+    res.render('login');
 });
 
 //get user by cuisine
@@ -16,6 +54,7 @@ usersRouter.get('/:cuisine', (req, res) => {
 //update
 usersRouter.post('/:name', userController.updateCuisineByName);
 usersRouter.patch('/', userController.addUser);
+
 
 //export the router so that app.js can import it
 module.exports = usersRouter;
--- app.js
+++ app.js
@@ -1,25 +1,31 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
+
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 //pass the body first before router
 app.use(bodyParser.json()); //accept Json
 app.use(bodyParser.urlencoded({extended: true})); //accept url
 
+app.get('/', (req, res) => {
+    //return the response
+    res.render('index');
+});
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //import routers
 const userRouter = require('./Routers/userRouter');
 
-app.get('/', (req, res) => {
-    //return the response
-    res.send("<H1>Welcome to Mit&Yit.</H1>");
-});
-
 // go through the router first before doing the following
 app.use('/user', userRouter);
-
 app.use('/user/:name', userRouter);
-
 
 
 //app.listen() tells express that we want to start a web server on port 8000

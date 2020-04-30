b58085b0338ed7a0a895ce57ae387b1dd337632a
GilfoylePP <61959092+GilfoylePP@users.noreply.github.com>
2020-04-30 16:31:17 +1000

    Update matchRouter.js

--- routes/matchRouter.js
+++ routes/matchRouter.js
@@ -2,13 +2,13 @@ const express = require('express');
 const matchRouter = express.Router();
 const matchController = require("../controllers/matchController.js");
 
-// find matches for an user (tested)
+// find matches for a user (tested)
 matchRouter.get('/:id', matchController.findMatch);
 
-// find matches for an user by cuisine (tested)
+// find matches for a user by cuisine (tested)
 matchRouter.get('/find_cuisine/:id', matchController.ByCuisine);
 
-// find matches for an user by availability (tested)
+// find matches for a user by availability (tested)
 matchRouter.get('/find_availability/:id', matchController.ByAvailability);
 
 module.exports = matchRouter;   
